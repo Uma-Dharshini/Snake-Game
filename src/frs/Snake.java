5d8245f39785c2f12734b8a7d9329556df9e99f0
Uma-Dharshini <umadharshini013@gmail.com>
2024-06-24 22:35:20 +0530

    First Commit

---
+++ src/frs/Snake.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frs;
+
+/**
+ *
+ * @author 91790
+ */
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
